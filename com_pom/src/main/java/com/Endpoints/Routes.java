75e138238a520c94419d5bf2b1c11b57107e249c
prasanna <tunga.prasanna@swiggy.in>
2024-06-13 20:20:18 +0530

    dummy commit

--- com_pom/src/main/java/com/Endpoints/Routes.java
+++ com_pom/src/main/java/com/Endpoints/Routes.java
@@ -1,9 +1,9 @@
-package com.endpoints;
+package com.Endpoints;
 
 public class Routes {
     public static String base_url="https://jsonplaceholder.typicode.com";
     public static String users_url=base_url+"/users";
-    public static String todos_url=base_url+"/todos";
+    public static String todos_url=base_url+"/todos"; 
 
 
 
