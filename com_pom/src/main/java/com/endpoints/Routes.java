8602a03e665be511e8360b353a33a1cbe9370a6a
Lakshmi Prasanna Tunga <95290499+prasannatunga23@users.noreply.github.com>
2024-06-14 08:12:20 +0530

    Update Routes.java
    dummy commit

--- com_pom/src/main/java/com/endpoints/Routes.java
+++ com_pom/src/main/java/com/endpoints/Routes.java
@@ -1,7 +1,7 @@
 package com.endpoints;
 
 public class Routes {
-    public static String base_url="https://jsonplaceholder.typicode.com";
+    public static String base_url="https://jsonplaceholder.typicode.com"; //base_url
     public static String users_url=base_url+"/users";
     public static String todos_url=base_url+"/todos"; 
 
